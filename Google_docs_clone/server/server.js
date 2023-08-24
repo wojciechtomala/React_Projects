1beb5a2e3122e4c3751a72f16079cefd2b21d3b6
Wojciech Tomala <105435994+wojciechtomala@users.noreply.github.com>
2023-08-24 12:12:31 +0200

    Add files via upload

---
+++ Google_docs_clone/server/server.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Document = require('./Document');
+
+mongoose.connect("mongodb://127.0.0.1:27017/google-docs-clone");
+
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST']
+    }
+});
+
+io.on("connection", socket => {
+    socket.on("get-document", async documentId => {
+      const document = await findOrCreateDocument(documentId)
+      socket.join(documentId);
+      socket.emit("load-document", document.data);
+
+      socket.on("send-changes", delta => {
+        socket.broadcast.to(documentId).emit("receive-changes", delta)
+      });
+  
+      socket.on("save-document", async data => {
+        await Document.findByIdAndUpdate(documentId, { data })
+      });
+    })
+  })
+
+const defaultValue = "";
+
+async function findOrCreateDocument(id){
+    if(id == null) return;
+    const document = await Document.findById(id);
+    if(document) return document;
+    return await Document.create({_id: id, data: defaultValue});
+}
